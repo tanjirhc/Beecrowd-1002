42256a0a8c3f49e391963605f4e793c749e53a01
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-03-08 01:01:15 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beecrowd_1002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Radius: ");
+            double R = Convert.ToDouble(Console.ReadLine());
+            double A = Math.PI * R * R;
+            Console.WriteLine("Area of circle is: \n" + A);
+
+            Console.ReadLine();
+        }
+    }
+}
